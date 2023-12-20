7128b22231614e69c7e2008cf90a5b3ff454a619
Juan Elvir <jelvir97@gmail.com>
2023-12-20 11:15:56 -0600

    shuffling cards

--- src/Deck.js
+++ src/Deck.js
@@ -7,6 +7,7 @@ const Deck = ()=>{
     const [baseUrl, setBaseUrl] = useState(`https://deckofcardsapi.com/api/deck/`)
     const [cards, setCards] = useState([])
     const [cardsRemaining, setCardsRemaining] = useState(52)
+    const [shuffling, setShuffling] = useState(false)
 
     useEffect(()=>{
         console.log('using effect')
@@ -17,22 +18,36 @@ const Deck = ()=>{
     const drawCard = async ()=>{
         try{
             const {data} = await axios.get(baseUrl+deckId+"/draw/?count=1")
-            console.log(data.cards[0].image)
-            setCards([...cards, data.cards[0].image])
+
+            setCards([...cards, {img: data.cards[0].image, code: data.cards[0].code}])
             setCardsRemaining(data.remaining)
         }catch(err){
             alert('There are no more cards!')
         }
     }
 
-    
+    const shuffleCards = async ()=>{
+        try{
+            setShuffling(true)
+            const {data} = await axios.get(baseUrl+deckId+"/shuffle")
+            setCardsRemaining(data.remaining)
+            setCards([])
+            
+        }catch(err){
+            alert('There was an error')
+        }finally{
+            setShuffling(false)
+        }
+    }
+
 
     return (
         <div className="Deck">
-            <button className="Deck-DrawButton" onClick={drawCard}> Draw</button>
+            <button className="Deck-DrawButton" onClick={drawCard} disabled={shuffling}>Draw</button>
+            <button className="Deck-ShuffleButton" onClick={shuffleCards} disabled={shuffling}>{shuffling? '...loading':"Shuffle"}</button>
             <div className="Deck-Container">
                 <h3>Cards Remaining: {cardsRemaining}</h3>
-                {cards.map(c => <Card url={c} />)}
+                {cards.map(c => <Card url={c.img} key={c.code}/>)}
             </div>
         </div>
     )
